611d6d138c4af8fc31d836103bca2d0032f80396
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-06-27 16:19:29 -0600

    idk

--- gui/MainWindow.xaml.cs
+++ gui/MainWindow.xaml.cs
@@ -1,13 +1,12 @@
-﻿using System.Text;
+﻿using d9.utl;
+using Modrinth;
+using Modrinth.Models;
+using Modrinth.Models.Tags;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using File = System.IO.File;
 
 namespace d9.lcm;
 /// <summary>
@@ -18,5 +17,49 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    private async void LoadMods_Click(object sender, RoutedEventArgs _)
+    {
+        string basePath = @"C:\Users\dninemfive\Documents\workspaces\mods\_meta\d9.lcm";
+        string[] rows = File.ReadAllLines($@"{basePath}\examplemods.txt");
+        using FileStream fs = File.OpenWrite($@"{basePath}\output.txt");
+        using StreamWriter sw = new(fs);
+        ModrinthClientConfig mcc = new()
+        {
+            UserAgent = "dninemfive-lcm/0.0.0"
+        };
+        using ModrinthClient client = new(mcc);
+        List<GameVersion> projects = new();
+        ModList.ItemsSource = projects;
+        IEnumerable<GameVersion> versions = await client.Tag.GetGameVersionsAsync();
+        foreach (GameVersion gv in versions)
+        {
+            projects.Add(gv);
+        }
+        projects = projects.OrderByDescending(x => x.Date).ToList();
+        /*
+        foreach (string row in rows)
+        {
+            try
+            {
+                Project project = await client.Project.GetAsync(row.Split("/").Last());
+                string summary = $"{project.Title}\t{project.GameVersions.ListNotation()}";
+                sw.WriteLine(summary);
+                Print(summary);
+                projects.Add(project);
+            }
+            catch(Exception e)
+            {
+                sw.WriteLine(e);
+                Print(e);
+            }
+        }*/
+    }
+    public void Print(object? obj)
+    {
+        Run run = new($"{obj}");
+        Paragraph result = new();
+        result.Inlines.Add(run);
+        // Document.Blocks.Add(result);
     }
 }
---
+++ gui/MinecraftVersion.cs
@@ -0,0 +1,7 @@
+﻿namespace d9.lcm.gui;
+public record MinecraftVersion(string Name, DateTime ReleaseTime)
+    : IComparable<MinecraftVersion>
+{
+    public int CompareTo(MinecraftVersion? other)
+        => ReleaseTime.CompareTo(other?.ReleaseTime);
+}
