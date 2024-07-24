de84b5da777221f9a67fbd99285a06382c706e48
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-07-24 11:27:59 -0600

    spreadsheet stuff i forgor to commit

---
+++ lib/spreadsheets/ColumnAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d9.lcm;
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public class ColumnAttribute(int index, string? name = null) : Attribute
+{
+    public int Index = index;
+    public string? Name = name;
+}
--- lib/spreadsheets/SpreadsheetExtensions.cs
+++ lib/spreadsheets/SpreadsheetExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using d9.utl;
 
 namespace d9.lcm;
 public static class SpreadsheetExtensions
@@ -10,5 +12,25 @@ public static class SpreadsheetExtensions
     public static Spreadsheet<T> ToSpreadsheetWithDelimiter<T>(this IEnumerable<string> lines, string delimiter)
         where T : IParsableWithDelimiter<T>
         => Spreadsheet<T>.LoadUsingDelimiter<T>(lines, delimiter);
-
+    public static string ToSpreadsheetRow(this object obj, string delimiter)
+    {
+        IEnumerable<(MemberInfo member, ColumnAttribute attr)> columns = obj.GetType().MembersWithAttribute<ColumnAttribute>();
+        if (columns.Select(x => x.attr.Index).Distinct().Count() < columns.Count())
+            throw new Exception("Duplicate column numbers on type {obj.GetType().Name}: {stuff idk}");
+        foreach(MemberInfo member in columns.OrderBy(x => x.attr.Index).Select(x => x.member))
+        {
+            if(member is FieldInfo field && field.GetValue(obj) is object fval)
+            {
+                
+            }
+            else if(member is PropertyInfo property && property.GetValue(obj, null) is object pval)
+            {
+                // write value
+            } else
+            {
+                // throw exception: invalid target
+            }
+        }
+        return ""; // joined values of above
+    }
 }
