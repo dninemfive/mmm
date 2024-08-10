9bc6679b17a2ead881c111067984200a55bf7889
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-08-10 09:23:02 -0600

    move AsyncCache to its own file

---
+++ lib/AsyncCache.cs
@@ -0,0 +1,16 @@
+﻿namespace d9.lcm;
+public class AsyncCache<K, V>(Func<K, Task<V>> getter)
+    where K : notnull
+{
+    private readonly Dictionary<K, V> _cache = new();
+    private readonly Func<K, Task<V>> _getter = getter;
+    public async Task<V> TryGet(K key)
+    {
+        if (_cache.TryGetValue(key, out V? value))
+            return value;
+        V? result = await _getter(key);
+        if (result is not null)
+            _cache[key] = result;
+        return result;
+    }
+}
--- lib/ModAnalysisContext.cs
+++ lib/ModAnalysisContext.cs
@@ -1,10 +1,5 @@
 ﻿using Modrinth;
 using Modrinth.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace d9.lcm;
 public class ModAnalysisContext(ModrinthClient client, MinecraftVersions versions)
@@ -19,19 +14,4 @@ public class ModAnalysisContext(ModrinthClient client, MinecraftVersions version
     }
     public AsyncCache<string, Project> ProjectCache = new(s => client.Project.GetAsync(s));
     
-}
-public class AsyncCache<K, V>(Func<K, Task<V>> getter)
-    where K : notnull
-{
-    private Dictionary<K, V> _cache = new();
-    private Func<K, Task<V>> _getter = getter;
-    public async Task<V> TryGet(K key)
-    {
-        if (_cache.TryGetValue(key, out V? value))
-            return value;
-        V? result = await _getter(key);
-        if (result is not null)
-            _cache[key] = result;
-        return result;
-    }
 }
